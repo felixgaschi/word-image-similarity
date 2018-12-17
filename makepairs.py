0972996ca3cac1b78b4d90acd0b22e39e0e28930
felixgaschi <felix.gaschi@telecom-paristech.fr>
2018-12-17 13:06:37 +0100

    Added makepairs.py in order to build csv files with id of word images that are similar and others that are not

---
+++ makepairs.py
@@ -0,0 +1,81 @@
+"""
+file for constructing ground truth for similarity
+build a csv with true pairs (pair of different handwritting of same word)
+build another csv file with three columns:
+- id1: id of first word
+- id2: id of second word
+- similarity: similarity score (1 if same word, 0 otherwise)
+"""
+
+import argparse
+import os
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description='Word Image Similarity script for constructing ground truth')
+
+parser.add_argument("--data", type=str, default="../similarity-dataset", metavar="I",
+                    help="directory for the data")
+parser.add_argument("--limit", type=int, default=2000000, metavar="L",
+                    help="limit number of pairs")
+
+args = parser.parse_args()
+
+words = []
+
+with open(os.path.join(args.data, "words.txt")) as f:
+    words = [w.strip() for w in f.readlines()]
+
+if args.limit is None:
+    limit = len(words) * len(words)
+else:
+    limit = args.limit
+
+with open(os.path.join(args.data, "true-pairs.csv"), "w") as f:
+    f.write("")
+
+nb_true = 0
+pbar = tqdm(total=limit // 2)
+get_out = False
+for i, w in enumerate(words):
+    for j, w2 in enumerate(words):
+        if i != j and w == w2:
+            if nb_true < limit // 2:
+                nb_true += 1
+                with open(os.path.join(args.data, "true-pairs.csv"), "a") as f:
+                    f.write("{:06d},{:06d}\n".format(i + 1, j + 1))
+                pbar.update(1)
+            else:
+                get_out = True
+                break
+        if get_out:
+            pbar.close()
+            break
+if not get_out:
+    pbar.close()
+
+nb_false = limit - nb_true
+
+with open(os.path.join(args.data, "false-pairs.csv"), "w") as f:
+    f.write("")
+
+i, j, k = 0, 0, 0
+pbarfalse = tqdm(total=nb_false)
+while i < nb_false:
+    
+    if k == len(words):
+        j, k = j + 1, 0
+    
+    if j == len(words):
+        print("The process has restarted. There might be the same data twice in the dataset")
+        j, k = 0, 0
+    
+    if words[j] != words[k]:
+        i += 1
+        pbarfalse.update(1)
+        with open(os.path.join(args.data, "false-pairs.csv"), "a") as f:
+            f.write("{:06d},{:06d}\n".format(j + 1, k + 1))
+    
+    k += 1
+pbarfalse.close()
+
+print("Number of true pairs selected: ", nb_true)
