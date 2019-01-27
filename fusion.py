384baf724aba7ff5742d44bf4cb7d21baf36c0b6
felixgaschi <felix.gaschi@telecom-paristech.fr>
2019-01-27 16:24:02 +0100

    Fixed vision.py

--- fusion.py
+++ fusion.py
@@ -5,7 +5,7 @@ Created on Fri Jan 11 15:14:45 2019
 
 @author: combaldieu
 """
-import argparse
+import argparse, os
 import torch
 from time import time
 from tqdm import tqdm
@@ -50,6 +50,16 @@ parser.add_argument('--matching', type=str, default="strict",
 
 parser.add_argument('--nb-workers', type=int, default=1)
 
+parser.add_argument('--name', type=str, default=None)
+
+parser.add_argument('--save', dest="save", action="store_true")
+parser.add_argument('--no-save', dest="save", action="store_false")
+parser.set_defaults(save=False)
+
+parser.add_argument("--save-queries", dest="save_queries", action="store_true")
+parser.add_argument('--no-save-queries', dest="save_queries", action="store_false")
+parser.set_defaults(save_queries=False)
+
 
 args = parser.parse_args()
 
@@ -91,7 +101,7 @@ elif args.estimator2_type == "regressor":
 import data
 
 
-loader_controller = data.ValidationDataset(
+validation_set = data.ValidationDataset(
     args.data,
     begin=args.split,
     end=None,
@@ -105,58 +115,67 @@ loader_controller = data.ValidationDataset(
     matching=args.matching
 )
 
-train_loader = torch.utils.data.DataLoader(
-    loader_controller.training_set,
-    batch_size=args.batch_size, shuffle=True, num_workers=args.nb_workers
-)
 
 val_loader = torch.utils.data.DataLoader(
-    loader_controller.evaluation_set,
+    validation_set,
     batch_size=args.batch_size, shuffle=False, num_workers=args.nb_workers
 )
+
+dirName1 = args.model1_name
+dirName2 = args.model2_name
 
 if use_cuda:
     print('Using GPU')
     model1.cuda(args.gpu)
     model2.cuda(args.gpu)
+
+    model1.load_state_dict(torch.load(dirName1))
+    model2.load_state_dict(torch.load(dirName2))
 else:
     print('Using CPU')
     
-dirName1 = args.model1_name
-model1.load_state_dict(torch.load(dirName1))
+    model1.load_state_dict(torch.load(dirName1, map_location="cpu"))
+    model2.load_state_dict(torch.load(dirName2, map_location="cpu"))
+    
 model1.eval()
-
-dirName2 = args.model2_name
-model2.load_state_dict(torch.load(dirName2))
 model2.eval()
 
 def fusion():
-    retrieved = {}
-    relevantAndRetrieved = {}
+
+    queries = {}
+
+    nb_true = 0
+    nb_true_true = 0
+    nb_false = 0
+    nb_true_false = 0
+    nb_false_true = 0
+    nb_false_false = 0
 
     with torch.no_grad():
         model1.eval()
         model2.eval()
         fusion_loss = 0
         correct = 0
-        for data, target, indices in tqdm(val_loader):
+        for data, target, indices, img_indices in tqdm(val_loader):
             if use_cuda:
                 data, target = data.cuda(args.gpu), target.cuda(args.gpu)
             output1 = model1(data)
             output2 = model2(data)
             # sum up batch loss
             if args.estimator1_type == "class":
-                criterion1 = torch.nn.CrossEntropyLoss(reduction='mean')
+                criterion1 = torch.nn.CrossEntropyLoss(reduction='elementwise_mean')
+                target1 = target
             else:
-                criterion1 = torch.nn.MSELoss(reduction="mean")
+                criterion1 = torch.nn.MSELoss(reduction="elementwise_mean")
                 target1 = target.float()
                 output1 = output1[:,0]
             fusion_loss += criterion1(output1, target1).data.item()
             
             if args.estimator2_type == "class":
-                criterion2 = torch.nn.CrossEntropyLoss(reduction='mean')
+                criterion2 = torch.nn.CrossEntropyLoss(reduction='elementwise_mean')
+                target2 = target
             else:
-                criterion2 = torch.nn.MSELoss(reduction="mean")
+                criterion2 = torch.nn.MSELoss(reduction="elementwise_mean")
                 target2 = target.float()
                 output2 = output2[:,0]
             fusion_loss += criterion2(output2, target2).data.item()
@@ -176,38 +195,110 @@ def fusion():
                 #pred1 = output1.data.round()
                 pred1 = output1.data
                 
-            pred = (pred1 + pred2)/2
-            pred = pred.round()
+            score = (pred1 + pred2)/2
+            pred = score.round().long()
                 
             correct += pred.eq(target.data.view_as(pred)).cpu().sum()
             for j in range(indices.size(0)):
-                ref = int(indices[j, 0])
-                if ref not in retrieved.keys():
-                    retrieved[ref] = 0
-                if ref not in relevantAndRetrieved.keys():
-                    relevantAndRetrieved[ref] = 0
-                other = int(indices[j, 1])
+                query = int(img_indices[j, 0])
+                if query not in queries:
+                    queries[query] = []
+                queries[query].append((
+                    int(img_indices[j, 1]),
+                    score[j].cpu().item(), 
+                    target[j].cpu().item()
+                ))
 
-                if pred[j].cpu().item() == 1:
-                    retrieved[ref] += 1
-                if pred[j].cpu().item() == 1 and target[j].cpu().item() == 1:
-                    relevantAndRetrieved[ref] += 1
+                if target[j].cpu().item() == 1:
+                    nb_true += 1
+                    if pred[j].cpu().item() == 0:
+                        nb_false_false += 1
+                    else:
+                        nb_true_true += 1
+                else:
+                    nb_false += 1
+                    if pred[j].cpu().item() == 0:
+                        nb_true_false += 1
+                    else:
+                        nb_false_true += 1
 
-        scores = [relevantAndRetrieved[i] * 1. / retrieved[i] for i in retrieved.keys() if retrieved[i] > 0]
-        mAP = np.sum(scores) / len(scores)
+
+        mAP = 0
+        Q = 0
+        for q in queries.keys():
+            sorted_scores = sorted(queries[q], key=lambda x: x[1], reverse=True)
+            queries[q] = sorted_scores
+            p_nom = 0
+            p_div = 0
+            cum_sum = 0
+            nb_rel = 0
+            for s in sorted_scores:
+                if s[1] >= 0.5:
+                    p_div += 1
+                    if s[2] == 1:
+                        p_nom += 1
+                if s[2] == 1:
+                    cum_sum += p_nom * 1. / max(1, p_div)
+                    nb_rel += 1
+            score = cum_sum * 1. / max(1, nb_rel)
+            if nb_rel > 0:
+                mAP += score
+                Q += 1
+        mAP /= max(1, Q)
+        
+        true_precision = nb_true_true * 1. / (nb_true_true + nb_false_true) if nb_true_true > 0 else 0.
+        false_precision = nb_true_false * 1. / (nb_true_false + nb_false_false) if nb_true_false > 0 else 0.
 
         fusion_loss /= len(val_loader.dataset)
-        print('\nFusion set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%), mAP: {:.2f}%\n'.format(
+        print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%), mAP: {:.2f}%\n'.format(
             fusion_loss, correct, len(val_loader.dataset),
             100. * correct / len(val_loader.dataset),
             100. * mAP
         ))
     
-        return 100. * correct / len(val_loader.dataset)
+        return 100. * correct / len(val_loader.dataset), fusion_loss, mAP, nb_true_true * 1. / nb_true, nb_true_false * 1. / nb_false, true_precision, false_precision, queries
     
+
+if args.save:
+    if not os.path.exists(args.experiment):
+        os.makedirs(args.experiment)
+        dirName = "exp-00000"
+    else:
+        prefixes = [int(d[4:9]) for d in os.listdir(args.experiment) if d[:4] == "exp-"]
+        maxi = 0 if len(prefixes) == 0 else max(prefixes)
+        dirName = "exp-{:05d}".format(maxi + 1)
+    if args.name is not None:
+        dirName += "-" + args.name
+    os.makedirs(os.path.join(args.experiment, dirName))
+    with open(os.path.join(args.experiment, dirName, "info"), "w") as f:
+        dict = vars(args)
+        res = "\n".join(["{}: {}".format(e, dict[e]) for e in dict.keys()]) + "\n"
+        f.write(res)
+    with open(os.path.join(args.experiment, dirName, "scores.csv"), "w") as f:
+        f.write("val_acc, val_loss, time, mAP, acc_true, acc_false, true_P, false_P\n")
+
     
-for _ in range(10):
-    t = time()
-    fusion_score = fusion()
-    elapsed_time = time() - t
-    print("Elapsed time: ", elapsed_time)
+
+t = time()
+test_score, loss, mAP, acc_true, acc_false, true_P, false_P, queries = fusion()
+if args.save_queries:
+    if args.save:
+        path = os.path.join(args.experiment, dirName, "queries.txt")
+    else:
+        path = os.path.join(args.experiment, "queries.txt")
+    with open(path, "w") as f:
+        for q in sorted(queries.keys()):
+            line = ""
+            line += "{}".format(q)
+            for value in queries[q]:
+                line += ",{}".format(value)
+            line += "\n"
+            f.write(line)
+
+elapsed_time = time() - t
+print("Elapsed time: ", elapsed_time)
+
+if args.save:
+    with open(os.path.join(args.experiment, dirName, "scores.csv"), "a") as f:
+        f.write("{:f},{:f},{:.2f},{:f},{:f},{:f},{:f},{:f}\n".format(test_score, loss, elapsed_time, mAP, acc_true, acc_false, true_P, false_P))
+        
