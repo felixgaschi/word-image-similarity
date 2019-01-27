Fixed resnet classifier, changed sigmoid for softmax

models.py
@@ -246,5 +246,5 @@ class Resnet50Classifier(nn.Module):
 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = F.softmax(self.fc3(x), dim=1)
         return x